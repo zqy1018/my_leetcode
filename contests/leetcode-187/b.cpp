9677afc61f18138d262eddb48d702284ff7c9fdd
zqy1018 <979115525@qq.com>
2020-05-03 12:13:52 +0800

    added weekly 187

---
+++ contests/leetcode-187/b.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+#define INF 2000000000
+using namespace std;
+typedef long long ll;
+int read(){
+    int f = 1, x = 0;
+    char c = getchar();
+    while(c < '0' || c > '9'){if(c == '-') f = -f; c = getchar();}
+    while(c >= '0' && c <= '9')x = x * 10 + c - '0', c = getchar();
+    return f * x; 
+}
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+bool isLeaf(TreeNode* root) {
+    return root->left == NULL && root->right == NULL;
+}
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+template<typename T>
+ostream& operator << (ostream& os, const vector<T>& vec){
+    for (auto x: vec)
+        os << x << " ";
+    os << endl;
+    return os;
+}
+
+template<typename T>
+ostream& operator << (ostream& os, const vector<vector<T>>& vec){
+    for (auto& v: vec){
+        for (auto x: v)
+            os << x << " ";
+        os << endl;
+    }
+    return os;
+}
+class Solution {
+public:
+    bool kLengthApart(vector<int>& nums, int k) {
+        int n = nums.size();
+        int lst = -100000;
+        for (int i = 0; i < n; ++i){
+            if (nums[i] == 1){
+                if (i - lst - 1 < k) return false;
+                lst = i;
+            }
+            
+        }
+        return true;
+    }
+};
+Solution sol;
+void init(){
+    
+}
+void solve(){
+    // sol.convert();
+}
+int main(){
+    init();
+    solve();
+    return 0;
+}
