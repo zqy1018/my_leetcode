f4b3c0b7444358e1813dffbd42820150e4a9c998
zqy1018 <979115525@qq.com>
2020-05-03 11:41:54 +0800

    added week 187

---
+++ contests/leetcode-187/c.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+#define INF 2000000000
+using namespace std;
+typedef long long ll;
+int read(){
+    int f = 1, x = 0;
+    char c = getchar();
+    while(c < '0' || c > '9'){if(c == '-') f = -f; c = getchar();}
+    while(c >= '0' && c <= '9')x = x * 10 + c - '0', c = getchar();
+    return f * x; 
+}
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+bool isLeaf(TreeNode* root) {
+    return root->left == NULL && root->right == NULL;
+}
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+template<typename T>
+ostream& operator << (ostream& os, const vector<T>& vec){
+    for (auto x: vec)
+        os << x << " ";
+    os << endl;
+    return os;
+}
+
+template<typename T>
+ostream& operator << (ostream& os, const vector<vector<T>>& vec){
+    for (auto& v: vec){
+        for (auto x: v)
+            os << x << " ";
+        os << endl;
+    }
+    return os;
+}
+
+class Solution {
+    multiset<int> st;
+public:
+    int longestSubarray(vector<int>& nums, int limit) {
+        int n = nums.size();
+        int ans = 0;
+        for (int i = 0, cur = 0; i < n; ++i){
+            while (cur < n && (st.empty() || *st.rbegin() - *st.begin() <= limit))
+                st.insert(nums[cur]), ++cur;
+            int tt = cur - i - 1;
+            ans = max(ans, tt);
+            st.erase(st.find(nums[i]));
+        }
+        return ans;
+    }
+};
+Solution sol;
+void init(){
+    
+}
+void solve(){
+    // sol.convert();
+}
+int main(){
+    init();
+    solve();
+    return 0;
+}
