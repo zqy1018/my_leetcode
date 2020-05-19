37b8deb28821266dcdeda1ca773bbaa699bf8551
zqy1018 <979115525@qq.com>
2020-05-19 20:46:52 +0800

    added 680

---
+++ code/680.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+#define INF 2000000000
+using namespace std;
+typedef long long ll;
+int read(){
+    int f = 1, x = 0;
+    char c = getchar();
+    while(c < '0' || c > '9'){if(c == '-') f = -f; c = getchar();}
+    while(c >= '0' && c <= '9')x = x * 10 + c - '0', c = getchar();
+    return f * x; 
+}
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+bool isLeaf(TreeNode* root) {
+    return root->left == NULL && root->right == NULL;
+}
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+template<typename T>
+ostream& operator << (ostream& os, const vector<T>& vec){
+    for (auto x: vec)
+        os << x << " ";
+    os << endl;
+    return os;
+}
+
+template<typename T>
+ostream& operator << (ostream& os, const vector<vector<T>>& vec){
+    for (auto& v: vec){
+        for (auto x: v)
+            os << x << " ";
+        os << endl;
+    }
+    return os;
+}
+
+class Solution {
+    bool judge(int i, int j, bool used, string& s){
+        if (i >= j) return true;
+        if (s[i] == s[j]) return judge(i + 1, j - 1, used, s);
+        else {
+            if (used) return false;
+            return judge(i, j - 1, true, s) || judge(i + 1, j, true, s);
+        }
+    }
+public:
+    bool validPalindrome(string s) {
+        return judge(0, s.length() - 1, false, s);
+    }
+};
+Solution sol;
+void init(){
+    
+}
+void solve(){
+    // sol.convert();
+}
+int main(){
+    init();
+    solve();
+    return 0;
+}
